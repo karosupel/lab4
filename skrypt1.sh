1c01e95afca884d101887654e4a5082b3af8351f
Karolina <karosupel@gmail.com>
2025-04-22 14:24:21 +0200

    Drugi myslnik

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-if [ "$1" = "--date" ]; then
+if [ "$1" = "--date" ]
+then
 	echo "$(date)"
+elif [ "$1" = "--logs" ]
+then
+	#liczba=1
+	for ((i=1; i<=100; i++))
+	do
+		touch log$i.txt
+	done
 else
 	echo "Prosze podac odpowiednia flage"
 fi
