348ac6ff14ec56daa798baaae793cf66b6ad9329
Karolina <karosupel@gmail.com>
2025-04-22 14:56:03 +0200

    Trzeci podpunkt

--- skrypt1.sh
+++ skrypt1.sh
@@ -3,13 +3,18 @@
 if [ "$1" = "--date" ]
 then
 	echo "$(date)"
-elif [ "$1" = "--logs" ]
+elif [ "$1" = "--logs" ] && [ "$2" = "" ]
 then
-	#liczba=1
-	for ((i=1; i<=100; i++))
+	for ((i=1; i<=5; i++))
 	do
 		touch log$i.txt
 	done
+elif [ "$1" = "--logs" ] && [ "$2" -gt 0 ]
+then
+	for ((i=1; i<="$2"; i++))
+        do
+                touch log$i.txt
+        done
 else
 	echo "Prosze podac odpowiednia flage"
 fi
