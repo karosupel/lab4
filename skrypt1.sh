940ffc9eb1ee0e8bdd90fd91645e2b6896025cc4
Karolina <karosupel@gmail.com>
2025-04-25 11:29:12 +0200

    Myslnik 1 projekt 2

--- skrypt1.sh
+++ skrypt1.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-if [ "$1" = "--date" ]
+if [ "$1" = "--date" ] || [ "$1" = "-d" ]
 then
 	echo "$(date)"
-elif [ "$1" = "--logs" ] && [ "$2" = "" ]
+elif [ "$1" = "--logs" ] || [ "$1" = "-l" ] && [ "$2" = "" ]
 then
-	for ((i=1; i<=5; i++))
+	for ((i=1; i<=100; i++))
 	do
 		touch log$i.txt
 	done
-elif [ "$1" = "--logs" ] && [ "$2" -gt 0 ]
+elif [ "$1" = "--logs" ] || [ "$1" = "-l" ] && [ "$2" -gt 0 ]
 then
 	for ((i=1; i<="$2"; i++))
         do
                 touch log$i.txt
         done
-elif [ "$1" = "--help" ]
+elif [ "$1" = "--help" ] || [ "$1" = "-h" ]
 then
 	echo "Dostepne komedy to: "
 	echo "--date - wyswietla dzisiejsza date"
